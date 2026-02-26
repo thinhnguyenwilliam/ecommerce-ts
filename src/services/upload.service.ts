285a306a4c22ebffbb1bd4ca5ed5b6ace9557c28
William-ticket-1 <thinhproee@gmai.com>
2026-02-26 16:04:23 +0700

    ft: aws link

--- src/services/upload.service.ts
+++ src/services/upload.service.ts
@@ -1,5 +1,9 @@
 // ecommerce-ts/src/services/upload.service.ts
-import { PutObjectCommand } from "@aws-sdk/client-s3";
+import {
+  PutObjectCommand,
+  GetObjectCommand,
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import s3Client from "../config/s3.config";
 import config from "../config/environment";
 
@@ -8,14 +12,25 @@ export const uploadToS3 = async (
   key: string,
   contentType: string
 ) => {
-  const command = new PutObjectCommand({
+  // Upload
+  await s3Client.send(
+    new PutObjectCommand({
+      Bucket: config.aws.s3Bucket,
+      Key: key,
+      Body: buffer,
+      ContentType: contentType,
+    })
+  );
+
+  // Signed URL để xem ảnh
+  const getCommand = new GetObjectCommand({
     Bucket: config.aws.s3Bucket,
     Key: key,
-    Body: buffer,
-    ContentType: contentType,
   });
 
-  await s3Client.send(command);
+  const signedUrl = await getSignedUrl(s3Client, getCommand, {
+    expiresIn: 60 * 60, // 1 giờ
+  });
 
-  return `https://${config.aws.s3Bucket}.s3.${config.aws.region}.amazonaws.com/${key}`;
+  return { signedUrl };
 };
